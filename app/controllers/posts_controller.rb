88be8aa86ec5e8e435ae05c515a8a710076772d2
cdg77 <cdegregorio@gmail.com>
2015-10-17 17:16:05 -0700

    create a new blog post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,5 +8,23 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     render :show
   end
-  
+
+  def new
+    @post = Post.new
+    render :new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :author, :images, :body)
+  end
 end
