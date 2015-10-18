844ce6df30bec60a973cc5b6f010d73bc0b649fd
cdg77 <cdegregorio@gmail.com>
2015-10-18 09:15:25 -0700

    edit, update and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,27 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  # Note: The private method should be at the bottom of your controller code and only needs to be written once to be used by the create and update actions. Any code after private will not be accessible externally
   private
   def post_params
     params.require(:post).permit(:title, :author, :images, :body)
