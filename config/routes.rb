c93b61ab153ea0808dfd96b0c1eb997146fff8ea
cdg77 <cdegregorio@gmail.com>
2015-10-24 19:34:51 -0700

    Fixed logout bug.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root :to => 'posts#index'
-  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  # devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
 
   resources :posts do
     resources :tags, :except => [:show, :index]
